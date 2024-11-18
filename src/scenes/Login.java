5150589626f929138378b36728107bcff4f00a11
Khankw <jskw247@gmail.com>
2024-11-18 18:15:59 +0900

    Update Login & Signin

--- src/scenes/Login.java
+++ src/scenes/Login.java
@@ -7,20 +7,20 @@ public class Login extends Scene {
 	@Override
 	public void update() {
 		printTitle("로그인");
-		String id= in.input("ID");
-		String pw= in.input("PW");
-		
+		String id = in.input("ID");
+		String pw = in.input("PW");
+
 		UserManager usermanager = UserManager.getInstance();
-		
-		if(usermanager.login(id, pw)) {
+
+		if (usermanager.login(id, pw)) {
 			out.add(usermanager.getUserId(), color.BRIGHTBLUE);
 			out.resetColor();
 			out.println("님, 로그인 완료되었습니다.");
-		}else {
-			out.add("사용자 정보를 확인해 주세요.", color.RED);
-			out.resetColor();
-			out.println();
-		}
+			manager.changeScene("Lobby");
+			return;
+		} else
+			out.printErrln("사용자 정보를 확인해 주세요.");
+
 		manager.prevScene();
 	}
 }
--- src/scenes/Signin.java
+++ src/scenes/Signin.java
@@ -6,20 +6,18 @@ public class Signin extends Scene {
 	@Override
 	public void update() {
 		printTitle("가입");
-		String id= in.input("ID");
-		String pw= in.input("PW");
-		
+		String id = in.input("ID");
+		String pw = in.input("PW");
+
 		UserManager usermanager = UserManager.getInstance();
-		
-		if(usermanager.signin(id, pw)) {
+
+		if (usermanager.signin(id, pw)) {
 			out.add(id, color.BRIGHTBLUE);
 			out.resetColor();
 			out.println("님의 회원가입이 완료되었습니다.");
-		}else {
-			out.add("ID가 중복되었습니다.", color.RED);
-			out.resetColor();
-			out.println();
-		}
+		} else
+			out.printErrln("ID가 중복되었습니다.");
+
 		manager.prevScene();
 	}
 }
