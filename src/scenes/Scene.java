c6b6c37b79d649bdcfecb37900fe96b4fb0cde45
Khankw <jskw247@gmail.com>
2024-11-18 17:41:59 +0900

    Update Scene

--- src/scenes/Scene.java
+++ src/scenes/Scene.java
@@ -1,10 +1,40 @@
 package scenes;
 
+import framework.*;
+import textrpg.TextRPG;
+
 abstract public class Scene {
-	private Scene prevScene;
+	protected final int EXIT = 0;
+
+	protected IndexColor color;
+	protected Output out = Output.getInstance();
+	protected Input in = Input.getInstance();
+
+	protected TextRPG game = TextRPG.getInstance();
+
+	protected SceneManager manager = SceneManager.getInstance();
+
+	protected Scene prevScene;
 
 	public void setPrevScene(Scene prevScene) {
-		this.prevScene = prevScene;
+		if (prevScene != null)
+			this.prevScene = prevScene;
+	}
+
+	public Scene getPrevScene() {
+		return this.prevScene;
+	}
+
+	
+	protected void printTitle(String title) {
+		out.add("======== ", color.GREEN, color.BRIGHTYELLOW);
+		out.add(title);
+		out.println(" ========");
+		out.resetColor();
+	}
+
+	protected void exit() {
+		game.exit();
 	}
 
 	abstract public void update();
