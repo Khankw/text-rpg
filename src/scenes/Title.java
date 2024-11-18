f6adc4c36faa54549a7b1ea180bdaef1e5d6fb2a
Khankw <jskw247@gmail.com>
2024-11-18 17:44:17 +0900

    Create Title.java

---
+++ src/scenes/Title.java
@@ -0,0 +1,19 @@
+package scenes;
+
+public class Title extends Scene {
+	private final int LOGIN = 1;
+	private final int SIGNIN = 2;
+
+	@Override
+	public void update() {
+		printTitle("Text RPG");
+		int sel = in.input("[1]로그인 [2]가입", -1);
+
+		if (sel == LOGIN)
+			manager.changeScene("Login");
+		else if (sel == SIGNIN)
+			manager.changeScene("Signin");
+		else if (sel == EXIT)
+			exit();
+	}
+}
