c542ae34f9b73b6008eef81247fe67f7886640ba
Khankw <jskw247@gmail.com>
2024-11-18 17:45:19 +0900

    Update TextRPG

--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -1,5 +1,32 @@
 package textrpg;
 
-public class TextRPG {
+import scenes.*;
 
+public class TextRPG {
+	private TextRPG() {}
+	private static TextRPG instance = new TextRPG();
+	public static TextRPG getInstance() {
+		return instance;
+	}
+	
+	private SceneManager scene = SceneManager.getInstance();
+	
+	private boolean isRun = true;
+	
+	public void run() {
+		initGame();
+		while(isRun) {
+			scene.update();
+		}
+	}
+
+	private void initGame() {
+		scene.putScene(new Title());
+		scene.putScene(new Login());
+		scene.putScene(new Signin());
+	}
+	
+	public void exit() {
+		isRun= false;
+	}
 }
