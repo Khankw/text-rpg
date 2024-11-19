c4b387c6ef2b851ea314c06f26c364de7fea5d0b
Khankw <jskw247@gmail.com>
2024-11-19 15:30:18 +0900

    Update TextRPG

--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -21,10 +21,21 @@ public class TextRPG {
 	}
 
 	private void initGame() {
-		scene.putScene(new Title());
-		scene.putScene(new Login());
-		scene.putScene(new Signin());
-		scene.putScene(new Lobby());
+		StringBuilder className = new StringBuilder();
+		String[] scenes = {"Title","Login","Signin","Lobby",
+				"Dungeon","Party","Guild","Store"};
+
+		for(int i= 0;i<scenes.length;i++) {
+			className.setLength(0);
+			className.append("scenes.").append(scenes[i]);
+			try {
+				Class<?> classScene = Class.forName(className.toString());
+				Scene newScene = (Scene) classScene.getConstructor().newInstance();
+				scene.putScene(newScene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void exit() {
