e0903ed8d9f60fc42fbe63e959753f05111da2ee
Khankw <jskw247@gmail.com>
2024-11-19 15:23:16 +0900

    Update Output all method return this

--- src/framework/Output.java
+++ src/framework/Output.java
@@ -26,93 +26,60 @@ public class Output {
 	private BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 	private StringBuffer str = new StringBuffer();
 
-	public void add(String msg) {
-		str.append(msg);
+	public Output add(Object obj) {
+		str.append(obj.toString());
+		return this;
 	}
 
-	public void add(int num) {
-		str.append(num);
-	}
-
-	public void add(String msg, IndexColor color) {
+	public Output add(Object obj, IndexColor color) {
 		addColor(color);
-		str.append(msg);
+		str.append(obj.toString());
+		return this;
 	}
 
-	public void add(int num, IndexColor color) {
-		addColor(color);
-		str.append(num);
-	}
-
-	public void add(String msg, IndexColor fColor, IndexColor bColor) {
+	public Output add(Object obj, IndexColor fColor, IndexColor bColor) {
 		addColor(fColor, bColor);
-		str.append(msg);
+		str.append(obj.toString());
+		return this;
 	}
 
-	public void add(int num, IndexColor fColor, IndexColor bColor) {
-		addColor(fColor, bColor);
-		str.append(num);
-	}
-
-	public void add(String msg, int r, int g, int b) {
+	public Output add(Object obj, int r, int g, int b) {
 		addColor(r, g, b);
-		str.append(msg);
+		str.append(obj.toString());
+		return this;
 	}
 
-	public void add(int num, int r, int g, int b) {
-		addColor(r, g, b);
-		str.append(num);
-	}
-
-	public void add(String msg, int fR, int fG, int fB, int bR, int bG, int bB) {
+	public Output add(Object obj, int fR, int fG, int fB, int bR, int bG, int bB) {
 		addColor(fR, fG, fB, bR, bG, bB);
-		str.append(msg);
+		str.append(obj.toString());
+		return this;
 	}
 
-	public void add(int num, int fR, int fG, int fB, int bR, int bG, int bB) {
-		addColor(fR, fG, fB, bR, bG, bB);
-		str.append(num);
-	}
-
-	public void addReset(String msg, IndexColor color) {
-		add(msg, color);
-	}
-
-	public void addReset(int num, IndexColor color) {
-		add(num, color);
-	}
-
-	public void addReset(String msg, IndexColor fColor, IndexColor bColor) {
-		add(msg, fColor, bColor);
+	public Output addReset(Object obj, IndexColor color) {
+		add(obj, color);
 		resetColor();
+		return this;
 	}
 
-	public void addReset(int num, IndexColor fColor, IndexColor bColor) {
-		add(num, fColor, bColor);
+	public Output addReset(Object obj, IndexColor fColor, IndexColor bColor) {
+		add(obj, fColor, bColor);
 		resetColor();
+		return this;
 	}
 
-	public void addReset(String msg, int r, int g, int b) {
-		add(msg, r, g, b);
+	public Output addReset(Object obj, int r, int g, int b) {
+		add(obj, r, g, b);
 		resetColor();
+		return this;
 	}
 
-	public void addReset(int num, int r, int g, int b) {
-		add(num, r, g, b);
+	public Output addReset(Object obj, int fR, int fG, int fB, int bR, int bG, int bB) {
+		add(obj, fR, fG, fB, bR, bG, bB);
 		resetColor();
+		return this;
 	}
 
-	public void addReset(String msg, int fR, int fG, int fB, int bR, int bG, int bB) {
-		add(msg, fR, fG, fB, bR, bG, bB);
-		resetColor();
-	}
-
-	public void addReset(int num, int fR, int fG, int fB, int bR, int bG, int bB) {
-		add(num, fR, fG, fB, bR, bG, bB);
-		resetColor();
-	}
-
-	public void addColor(IndexColor color) {
+	public Output addColor(IndexColor color) {
 		str.append(ESCAPE);
 		if (color.ordinal() < 8) {
 			str.append(FGCOLOR);
@@ -120,9 +87,10 @@ public class Output {
 			str.append(FGBLIGHT);
 		}
 		str.append(color.ordinal() % 8).append(END);
+		return this;
 	}
 
-	public void addColor(IndexColor fColor, IndexColor bColor) {
+	public Output addColor(IndexColor fColor, IndexColor bColor) {
 		str.append(ESCAPE);
 		if (fColor.ordinal() < 8) {
 			str.append(FGCOLOR);
@@ -136,27 +104,33 @@ public class Output {
 			str.append(BGBLIGHT);
 		}
 		str.append(bColor.ordinal() % 8).append(END);
+		return this;
 	}
 
-	public void addColor(int r, int g, int b) {
+	public Output addColor(int r, int g, int b) {
 		str.append(ESCAPE).append(FGCOLOR).append(RGB);
 		str.append(r).append(";").append(g).append(";").append(b).append(END);
+		return this;
 	}
 
-	public void addColor(int fR, int fG, int fB, int bR, int bG, int bB) {
+	public Output addColor(int fR, int fG, int fB, int bR, int bG, int bB) {
 		str.append(ESCAPE).append(FGCOLOR).append(RGB);
 		str.append(fR).append(";").append(fG).append(";").append(fB).append(";");
 		str.append(BGCOLOR).append(RGB);
 		str.append(bR).append(";").append(bG).append(";").append(bB).append(END);
-	}
-	public void resetColor() {
-		str.append(RESET);
+		return this;
 	}
 
-	public void addLine() {
-		str.append("\n");
+	public Output resetColor() {
+		str.append(RESET);
+		return this;
 	}
-	
+
+	public Output addLine() {
+		str.append("\n");
+		return this;
+	}
+
 	public void inputColor() {
 		resetColor();
 		str.append(ESCAPE).append(FGCOLOR).append(RGB);
@@ -164,44 +138,43 @@ public class Output {
 		print();
 	}
 
-	public void print(String msg) {
+	public Output print(String msg) {
 		str.append(msg);
 		print();
+		return this;
 	}
 
-	public void print(int num) {
+	public Output print(int num) {
 		str.append(num);
 		print();
+		return this;
 	}
 
-	public void println() {
+	public Output println() {
 		str.append("\n");
 		print();
+		return this;
 	}
 
-	public void println(String msg) {
-		str.append(msg).append("\n");
+	public Output println(Object obj) {
+		str.append(obj.toString()).append("\n");
 		print();
+		return this;
 	}
 
-	public void println(int num) {
-		str.append(num).append("\n");
-		print();
-	}
-
-	public void printErr(String msg) {
-		add(msg, IndexColor.RED);
+	public void printErr(Object obj) {
+		add(obj, IndexColor.RED);
 		resetColor();
 		print();
 	}
-	
-	public void printErrln(String msg) {
-		add(msg, IndexColor.RED);
+
+	public void printErrln(Object obj) {
+		add(obj, IndexColor.RED);
 		resetColor();
 		println();
 	}
-	
-	public void print() {
+
+	public Output print() {
 		try {
 			bufferedWriter.append(str);
 			str.setLength(0);
@@ -209,8 +182,9 @@ public class Output {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return this;
 	}
-	
+
 	@Override
 	public String toString() {
 		return str.toString();
