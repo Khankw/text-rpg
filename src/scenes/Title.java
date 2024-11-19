d28152b601695fd9b676426e58f12a2628eb7164
Khankw <jskw247@gmail.com>
2024-11-20 08:35:41 +0900

    Add Title login() & signin()

--- src/scenes/Title.java
+++ src/scenes/Title.java
@@ -7,15 +7,35 @@ public class Title extends Scene {
 	@Override
 	public void update() {
 		printTitle("Text RPG");
-		int sel = in.input("[1]로그인 [2]가입", -1);
+		int sel = in.input("[1]로그인 [2]가입 [0]종료", -1);
 
 		if (sel == LOGIN)
-			manager.changeScene("Login");
+			login();
 		else if (sel == SIGNIN)
-			manager.changeScene("Signin");
+			signin();
 		else if (sel == EXIT)
 			exit();
-		
+	}
 
+	private void login() {
+		printTitle("로그인");
+		String id = in.input("ID");
+		String pw = in.input("PW");
+
+		if (user.login(id, pw)) {
+			out.add(user.getUserId(), color.BRIGHTBLUE).reset().println("님, 로그인 완료되었습니다.");
+			player = user.getPlayer();
+			manager.changeScene("Lobby");
+		} else
+			out.printErrln("사용자 정보를 확인해 주세요.");
+	}
+
+	private void signin() {
+		printTitle("가입");
+		String id = in.input("ID");
+		String pw = in.input("PW");
+
+		if (user.signin(id, pw))
+			out.add(id, color.BRIGHTBLUE).reset().println("님의 회원가입이 완료되었습니다.");
 	}
 }
