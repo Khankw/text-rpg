ff96e8a047bfd2e07628a554b72d492a6636a463
Khankw <jskw247@gmail.com>
2024-11-19 18:07:12 +0900

    Update Guild & Party

--- src/scenes/Guild.java
+++ src/scenes/Guild.java
@@ -11,6 +11,7 @@ public class Guild extends Scene {
 	@Override
 	public void update() {
 		printTitle("길드");
+		out.println(user.getPlayer());
 		int sel = in.input("[1]용병모집 [2]용병해고 [0]로비", -1);
 
 		if (sel == RECRUIT)
@@ -22,24 +23,45 @@ public class Guild extends Scene {
 	}
 
 	private void recruit() {
+		int price = 10;
+
+		printTitle("용병모집");
 		Player player = user.getPlayer();
-		if (player.getMoney() < 1000) {
+		if (player.getMoney() < price) {
 			out.printErrln("모집할 금액이 부족합니다.");
 			return;
 		}
+		if(player.getUnitsSize()==20) {
+			out.printErrln("용병을 더 이상 늘릴수 없습니다.");
+			return;
+		}
+			
 
-		StringBuilder className = new StringBuilder();
+		StringBuilder str = new StringBuilder();
 		String[] unitClass = { "Warrior", "Cleric", "Mage" };
 
 		RandomGen ran = RandomGen.getInstance();
-		className.append("units.").append(unitClass[ran.get(unitClass.length)]);
+
+		str.append("units.").append(unitClass[ran.get(unitClass.length)]);
+		String classStr = str.toString();
+
+		str.setLength(0);
+		String[] firstNames = { "Sophia", "Liam", "Ava", "Benjamin", "Olivia", "Mason", "Mia", "Ethan", "Lucas",
+				"Charlotte", "Isaac", "Evelyn", "Wyatt", "Zoe", "David", "Amelia", "James", "Aiden", "Emma",
+				"Sebastian" };
+		String[] lastNames = { "Taylor", "Jordan", "Morgan", "Ryan", "Hunter", "Carter", "Avery", "Parker", "Austin",
+				"Madison", "Dylan", "Ellie", "Samantha", "Kennedy", "Sophie", "Adrian", "Riley", "Savannah", "Blake",
+				"Cameron" };
+
+		str.append(firstNames[ran.get(firstNames.length)]).append(" ").append(lastNames[ran.get(lastNames.length)]);
+		String name = str.toString();
 
 		try {
-			Class<?> classScene = Class.forName(className.toString());
-
+			Class<?> classScene = Class.forName(classStr);
 			Class<?>[] argTypes = { String.class };
-			Playable unit = (Playable) classScene.getConstructor(argTypes).newInstance(className.toString());
-			// player.subtractMoney(1000);
+			Playable unit = (Playable) classScene.getConstructor(argTypes).newInstance(name);
+			player.subtractMoney(price);
+			player.addUnit(unit);
 			out.addReset("#### 모집성공 ####\n", color.BLUE, color.BRIGHTGREEN);
 			out.add(unit).addLine().add(unit.getName()).println("이 고용되었습니다.");
 		} catch (Exception e) {
@@ -48,6 +70,26 @@ public class Guild extends Scene {
 	}
 
 	private void remove() {
-		out.println("해고");
+		printTitle("용병해고");
+		Player player = user.getPlayer();
+
+		if (player.getUnitsSize() < 2) {
+			out.printErrln("용병이 2명 이상일때 해고가 가능합니다.");
+			return;
+		}
+
+		out.println(player.unitsToString());
+		int sel = in.input("해고할 용병 번호 입력", 0) - 1;
+
+		if (sel < 0 || sel >= player.getUnitsSize()) {
+			out.printErrln("입력 범위를 벗어났습니다.");
+			return;
+		}
+
+		String name = player.getUnitName(sel);
+		player.removeUnit(sel);
+		out.addReset("#### 해고완료 ####\n", color.RED, color.BRIGHTRED);
+		out.add(name).println("이 해고되었습니다.");
+
 	}
 }
--- src/scenes/Party.java
+++ src/scenes/Party.java
@@ -1,17 +1,48 @@
 package scenes;
 
+import textrpg.Player;
+
 public class Party extends Scene {
+	private final int PARTY = 1;
+	private final int EQUIP = 2;
 
 	@Override
 	public void update() {
 		printTitle("파티");
-		int sel = in.input("[1]파티교체 [2]파티장비 [0]로비", -1);
-		
+		out.println(user.getPlayer());
+		int sel = in.input("[1]파티원교체 [2]장비교체 [0]로비", -1);
+
 		if (sel == 1)
-			out.println("파티교체");
-		else if (sel == 2)
-			out.println("파티장비");
+			party();
+		else if (sel == EQUIP)
+			equip();
 		else if (sel == EXIT)
 			manager.changeScene("Lobby");
+	}
+
+	private void party() {
+		Player player = user.getPlayer();
+
+		printTitle("파티원교체");
+		out.add(player.unitsToString()).add("\n");
+		out.println("0) 파티원 해제");
+		int index = in.input("용병 번호 입력", 0) - 1;
+		if (index < -1 || index >= player.getUnitsSize()) {
+			out.printErrln("입력 범위를 벗어났습니다.");
+			return;
+		}
+
+		int sel = in.input("파티 슬롯 선택", 0) - 1;
+		if (sel < 0 || sel >= 5) {
+			out.printErrln("입력 범위를 벗어났습니다.");
+			return;
+		}
+
+		player.setParty(index, sel);
+		out.println("파티원교체를 완료했습니다.");
+	}
+
+	private void equip() {
+
 	}
 }
