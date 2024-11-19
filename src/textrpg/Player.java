3d458164c5291355247edd9e5e386dab85f5912d
Khankw <jskw247@gmail.com>
2024-11-19 18:06:41 +0900

    Update Player

--- src/textrpg/Player.java
+++ src/textrpg/Player.java
@@ -1,7 +1,6 @@
 package textrpg;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import units.Playable;
 
@@ -12,31 +11,49 @@ public class Player {
 	private int money;
 
 	private ArrayList<Playable> units = new ArrayList<>();
-	private HashMap<Integer, Playable> party = new HashMap<>();
+	private ArrayList<Playable> party = new ArrayList<>();
 
 	public Player() {
 		this.money = 1500;
-		for (int i = 1; i <= MAXPARTY; i++) {
-			party.put(i, null);
-		}
+		for (int i = 0; i < MAXPARTY; i++)
+			party.add(null);
+
 	}
 
-	public void putParty(int index, Playable unit) {
-		if (index < 1 || index > MAXPARTY)
-			return;
-
-		party.put(index, unit);
+	public void addUnit(Playable unit) {
+		units.add(unit);
 	}
 
-	public void removeParty(int index) {
-		if (index < 1 || index > MAXPARTY)
-			return;
+	public void removeUnit(int index) {
+		Playable unit = units.get(index);
+		int idx = party.indexOf(unit);
+		if (idx != -1)
+			party.set(idx, null);
+		units.remove(index);
+	}
 
-		party.put(index, null);
+	public void setParty(int unitNum, int partyNum) {
+		Playable unit = null;
+		if (unitNum > -1)
+			unit = units.get(unitNum);
+
+		int idx = party.indexOf(unit);
+		if (idx != -1)
+			party.set(idx, null);
+
+		party.set(partyNum, unit);
 	}
 
 	public int getMoney() {
 		return money;
+	}
+
+	public String getUnitName(int index) {
+		return units.get(index).getName();
+	}
+
+	public int getUnitsSize() {
+		return units.size();
 	}
 
 	public void subtractMoney(int money) {
@@ -45,5 +62,35 @@ public class Player {
 
 	public void addMoney(int money) {
 		this.money += money;
+	}
+
+	public String unitsToString() {
+		StringBuilder str = new StringBuilder();
+		for (int i = 0; i < units.size(); i++) {
+			str.append(i + 1).append(") ").append(units.get(i));
+			if (i < units.size() - 1)
+				str.append("\n");
+		}
+
+		return str.toString();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append("[현금:").append(money).append("][유닛:");
+		str.append(units.size()).append("/").append(MAXUNITS).append("]\n");
+		for (int i = 0; i < MAXPARTY; i++) {
+			Playable unit = party.get(i);
+			str.append(i + 1).append("번째 용병----------------------\n");
+			if (unit == null)
+				str.append("빈슬롯");
+			else
+				str.append(unit);
+			str.append("\n");
+		}
+		str.append("------------------------------");
+
+		return str.toString();
 	}
 }
