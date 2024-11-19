394b1d6af5b79fd67bb0bb2514195647189d2b2b
Khankw <jskw247@gmail.com>
2024-11-20 08:37:57 +0900

    Refactor SceneManager & TextRPG init()

--- src/scenes/SceneManager.java
+++ src/scenes/SceneManager.java
@@ -18,16 +18,28 @@ public class SceneManager {
 
 	private Scene curScene;
 
-	public void putScene(Scene scene) {
-		scenes.put(scene.getClass().getSimpleName(), scene);
-		if (curScene == null)
-			curScene = scene;
+	public void init() {
+		StringBuilder className = new StringBuilder();
+		String[] sceneNames = { "Title", "Lobby", "Dungeon", "Party", "Guild", "Store", "Purchase" };
+
+		for (int i = 0; i < sceneNames.length; i++) {
+			className.setLength(0);
+			className.append("scenes.").append(sceneNames[i]);
+			try {
+				Class<?> classScene = Class.forName(className.toString());
+				Scene newScene = (Scene) classScene.getConstructor().newInstance();
+				scenes.put(sceneNames[i], newScene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		curScene = scenes.get("Title");
 	}
 
 	public void changeScene(String key) {
 		Scene newScene = scenes.get(key);
-		
-		if(newScene ==null) {
+
+		if (newScene == null) {
 			Output.getInstance().printErrln("해당 씬이 없음");
 			return;
 		}
--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -1,44 +1,29 @@
 package textrpg;
 
-import scenes.*;
+import scenes.SceneManager;
 
 public class TextRPG {
-	private TextRPG() {}
+	private TextRPG() {
+	}
+
 	private static TextRPG instance = new TextRPG();
+
 	public static TextRPG getInstance() {
 		return instance;
 	}
-	
+
 	private SceneManager scene = SceneManager.getInstance();
-	
+
 	private boolean isRun = true;
-	
+
 	public void run() {
-		initGame();
-		while(isRun) {
+		scene.init();
+		while (isRun) {
 			scene.update();
 		}
 	}
 
-	private void initGame() {
-		StringBuilder className = new StringBuilder();
-		String[] scenes = {"Title","Login","Signin","Lobby",
-				"Dungeon","Party","Guild","Store"};
-
-		for(int i= 0;i<scenes.length;i++) {
-			className.setLength(0);
-			className.append("scenes.").append(scenes[i]);
-			try {
-				Class<?> classScene = Class.forName(className.toString());
-				Scene newScene = (Scene) classScene.getConstructor().newInstance();
-				scene.putScene(newScene);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
 	public void exit() {
-		isRun= false;
+		isRun = false;
 	}
 }
